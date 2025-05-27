8cac0035942e3ad885d7f0fd4720f162a2ae5d17
Dann <Lesterdannlopez7@gmail.com>
2025-05-27 08:24:12 +0800

    Finished Exercise for Lesson 2

---
+++ Exercises/Lesson 2 Exercise.md
@@ -0,0 +1,5 @@
+## 🧠 Mini Challenges (Try These!)
+
+1. Write a function called `greetUser(name)` that prints `"Hello, NAME!"`
+2. Write a function that squares a number.
+3. Write an arrow function that checks if a number is even or odd.
---
+++ MyCode/Lesson 2/Lesson_2_exercise.js
@@ -0,0 +1,18 @@
+// Exercise 1: 
+function greetUser(name){
+    console.log("Hello, " + name + "!");
+}
+
+// Exercise 2:
+function squareNumber(num) {
+    return num * num;
+}
+
+// Exercise 3:
+function isEven(num) {
+    if (num % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
